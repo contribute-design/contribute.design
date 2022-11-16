8ee1b7a6f058b7ad28db566fc1e8e52cdaa49fad
fivenp <mh@fivenp.com>
2022-11-17 00:12:18 +0100

    feat: add analytics (#21)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { Provider as StyletronProvider } from 'styletron-react'
 import { BaseProvider } from 'baseui'
+import { Analytics } from '@vercel/analytics/react'
 
 import '../styles/globals.css'
 import { debug, styletron } from '../helpers/styletron'
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
       <BaseProvider theme={CustomTheme}>
         <Component {...pageProps} />
+        <Analytics />
       </BaseProvider>
     </StyletronProvider>
   )
