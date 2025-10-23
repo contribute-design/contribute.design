bf8b879461a12ea159195f1e2c3d8b66d6e0cd16
fivenp <mh@fivenp.com>
2025-10-23 14:22:19 +0200

    add redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/ubuntu',
+        destination: '/for-developers',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
